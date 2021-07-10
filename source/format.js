8ea85fe2ce001954e0924eb68803a033d9b4b35e
Siddharth <ahoy@barelyhuman.dev>
2021-07-11 03:39:52 +0530

    feat: additional formats and padding handlers

--- source/format.js
+++ source/format.js
@@ -1,13 +1,24 @@
-const selectorMap = {
-  DD: getDate,
-  MM: getMonth,
-  YYYY: getYear,
-  hh: getHours,
-  mm: getMinutes,
-  ss: getSeconds,
-};
+const addArg =
+  (fn, pad) =>
+  (...args) =>
+    fn(...args, pad);
 
-// Rethink the approach. for now bruteforce the format
+const selectorMap = {
+  DD: addArg(getDate, true),
+  MM: addArg(getMonth, true),
+  YYYY: getYear,
+  hh: addArg(getHours, true),
+  mm: addArg(getMinutes, true),
+  ss: addArg(getSeconds, true),
+
+  // Single letter matchers, added to the bottom
+  // to avoid them from replacing the above keys first
+  D: getDate,
+  M: getMonth,
+  h: getHours,
+  m: getMinutes,
+  s: getSeconds,
+};
 
 // format - allows you to use the following keys to handle simple
 // formatting of dates
@@ -16,35 +27,61 @@ const selectorMap = {
   DD: date 
   MM: month
   YYYY: year 
-  hh: hours
-  mm: minutes
+  hh: hour
+  mm: minute
   ss: seconds
+
+  D: date 
+  M: month
+  h: hour
+  m: minute
+  s: second
 */
+const regexBuilder = (key) => new RegExp(`${key}`);
 export function format(formatString, referenceDate = new Date()) {
-  return formatString
-    .replace(/DD/g, selectorMap["DD"](referenceDate))
-    .replace(/MM/g, selectorMap["MM"](referenceDate))
-    .replace(/YYYY/g, selectorMap["YYYY"](referenceDate))
-    .replace(/hh/g, selectorMap["hh"](referenceDate))
-    .replace(/mm/g, selectorMap["mm"](referenceDate))
-    .replace(/ss/g, selectorMap["ss"](referenceDate));
+  return Object.keys(selectorMap).reduce((acc, eachKey) => {
+    return acc.replace(
+      regexBuilder(eachKey),
+      selectorMap[eachKey](referenceDate)
+    );
+  }, formatString);
 }
 
-function getDate(referenceDate = new Date()) {
-  return new Date(referenceDate).getDate();
+function getDate(referenceDate = new Date(), pad = false) {
+  let val = new Date(referenceDate).getDate();
+  if (pad) {
+    val = Number(val) < 10 ? `0${val}` : val;
+  }
+  return val;
 }
-function getMonth(referenceDate = new Date()) {
-  return new Date(referenceDate).getMonth();
+function getMonth(referenceDate = new Date(), pad = false) {
+  let val = new Date(referenceDate).getMonth();
+  if (pad) {
+    val = Number(val) < 10 ? `0${val}` : val;
+  }
+  return val;
 }
 function getYear(referenceDate = new Date()) {
   return new Date(referenceDate).getFullYear();
 }
-function getHours(referenceDate = new Date()) {
-  return new Date(referenceDate).getHours();
+function getHours(referenceDate = new Date(), pad = false) {
+  let val = new Date(referenceDate).getHours();
+  if (pad) {
+    val = Number(val) < 10 ? `0${val}` : val;
+  }
+  return val;
 }
-function getMinutes(referenceDate = new Date()) {
-  return new Date(referenceDate).getMinutes();
+function getMinutes(referenceDate = new Date(), pad = false) {
+  let val = new Date(referenceDate).getMinutes();
+  if (pad) {
+    val = Number(val) < 10 ? `0${val}` : val;
+  }
+  return val;
 }
-function getSeconds(referenceDate = new Date()) {
-  return new Date(referenceDate).getSeconds();
+function getSeconds(referenceDate = new Date(), pad = false) {
+  let val = new Date(referenceDate).getSeconds();
+  if (pad) {
+    val = Number(val) < 10 ? `0${val}` : val;
+  }
+  return val;
 }
